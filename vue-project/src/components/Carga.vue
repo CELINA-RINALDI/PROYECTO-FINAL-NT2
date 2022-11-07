aaa00dd0b47880c963d33bdd5d216aaeae2c871a
Matías Pintow <gow.mt@hotmail.com>
2022-11-07 02:32:37 -0300

    Carga saldo en home

--- vue-project/src/components/Carga.vue
+++ vue-project/src/components/Carga.vue
@@ -2,5 +2,23 @@
     <div>Bienvenido a la vista de carga
         <router-link to="/home" class="boton mx-5"
             >Volver a Home</router-link>
+        <p>Monto: </p>
+        <input type="text" name="Saldo" placeholder="Saldo" />
+       <br>
+        <button @click="store.actualizarSaldo()">Cargar Saldo</button>
+
     </div>
 </template>
+<script>
+import { useStore } from  '../store/store';
+
+export default ({
+    setup() {
+   const store = useStore();
+   return {store}
+  },
+  async created(){
+    await this.store.init()
+  }
+})
+</script>
