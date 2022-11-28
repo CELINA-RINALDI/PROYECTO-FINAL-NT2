6bd3bf340984e04f89c59c87c6500508a5c74845
CELINA RINALDI <44110736@comunidadort.edu.ar>
2022-11-28 19:05:38 -0300

    cambios para presentar

--- vue-project/src/components/Ahorros.vue
+++ vue-project/src/components/Ahorros.vue
@@ -115,4 +115,8 @@ export default {
 .card:hover {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 }
+.container {
+    display: flex;
+    align-content: center;
+}
 </style>
--- vue-project/src/components/UserLogin.vue
+++ vue-project/src/components/UserLogin.vue
@@ -36,7 +36,6 @@ import { useStore } from '../store/store';
         methods: {
              validarLogin() {
               this.store.resetUsuario(); 
-              console.log('entro a validar')
           if (this.store.validarUsuario(this.input.username, this.input.password)) {
             console.log('valide')
             console.log(this.input.username)
